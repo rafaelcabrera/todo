4a691c366d171b5bdfad288fecb896f6d1ae6bad
Rafael <rafaelalejandrocabrera@gmail.com>
2022-01-26 22:47:03 -0300

    Manejo de estado- Ejemplo 1 bien claro en searchValue setSearchValue

--- todo_list/src/TodoSearch.js
+++ todo_list/src/TodoSearch.js
@@ -3,16 +3,26 @@ import './TodoSearch.css'
 
 function TodoSearch() {
   
+  const [searchValue, setSearchValue] = React.useState('');
+  // const [patito, setPatito] = React.useState('Patito')
+  // [
+  //   'Rafa',
+  //   setState('Alejandro')
+  // ]
+
   const onSearchValueChange = (e) => {
     console.log(e.target.value);
+    setSearchValue(e.target.value)
   }
-  return (
+  return [
       <input  className="TodoSearch" 
       type="text"
       placeholder="Insert New Task..."
+      value={searchValue}
       onChange={onSearchValueChange}
-      />
-  )
+      />,
+      <p>{searchValue}</p>
+  ];
 }
 
 export default TodoSearch;
