0f222d3a9ce807fbb0f6855dac512e6fe71ed8de
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-02-05 20:49:39 -0300

    Local storage

--- todo_list/src/App/index.js
+++ todo_list/src/App/index.js
@@ -1,53 +1,67 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from 'react';
-import {CreateTodoButton} from '../CreateTodoButton/CreateTodoButton';
-import {TodoCounter} from '../TodoCounter/TodoCounter';
-import {TodoItem} from '../TodoItem/TodoItem';
-import {TodoList} from '../TodoList/TodoList';
-import TodoSearch from '../TodoSearch/TodoSearch';
-
-import {AppUI} from './AppUI'
-
-const defaultTodos = [
-  {text:'Turno alergista' , completed: true},
-  {text:'Comprar monitor' , completed: false},
-  {text:'Dejar de gastar por 100 dias' , completed: false},
-  {text:'Cocinar las pechugas' , completed: false}
-]
+import { AppUI } from './AppUI';
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState('');
-  const [todos,setTodos] = React.useState([...defaultTodos]);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
 
-  const completedTodos = todos.filter(todo => !!todo.completed).length; //filtramos la propiedad completed y guardamos su longitud
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  
+  const [todos, setTodos] = React.useState(parsedTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
   let searchedTodos = [];
 
-  if(searchValue.lenght>=1){
-    searchedTodos=todos;
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
   }
-    else { 
-      searchedTodos = todos.filter(todo=>{
-        const todoText = todo.text.toLowerCase(); //llamamos a cada todo y lo convertios a minuscula
-        const searchText = searchValue.toLowerCase();
-        return todoText.includes(searchText); //filtro completo
-  })  
-}
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
   return (
     <AppUI
-    totalTodos={totalTodos}
-    completedTodos={completedTodos}
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-    searchedTodos={searchedTodos}
-
-
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
     />
-
-  )
+  );
 }
 
 export default App;
